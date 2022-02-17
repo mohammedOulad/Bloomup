ae6c4ebde164af0f442f37ad13644234f03302bc
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2022-02-17 15:21:17 +0100

    verdere uitleg bind/call/apply

--- LeerOnderwerpen/thisObjectandClosure/this/ThisFunctionExplicitBinding.js
+++ LeerOnderwerpen/thisObjectandClosure/this/ThisFunctionExplicitBinding.js
@@ -1,13 +1,43 @@
 const person = {
-     name: "mo",
-     age: 21
+    name: "mohammed",
+    age: 22
 }
 
-function printInfo() {
-    return `My name is ${this.name} & I am ${this.age} y/o`
+const person2 = {
+    name: "naim",
+    age: 85
 }
 
-const something = printInfo.call(person)
-//bind call en apply nog maken
+const person3 = {
+    name: "maurits",
+    age: 69
+}
 
-console.log(something);
+function printInfo(gemeente, stad) {
+    console.log(`My name is ${this.name} & I am ${this.age} y/o I live in ${gemeente} stad: ${stad}`)
+}
+
+console.log("\n call \n");
+
+//call with komma seperator
+printInfo.call(person, "borgerhout", "Antwerpen")
+printInfo.call(person2, "kiel")
+printInfo.call(person3, "deurne")
+
+console.log("\n apply \n");
+
+//apply within an array
+printInfo.apply(person, ["borgerhout" , "Antwerpen"])
+printInfo.apply(person2, ["kiel"])
+printInfo.apply(person3, ["deurne"])
+
+//bind 
+let printMoName = printInfo.bind(person, "borgerhout", "Antwerpen")
+let printNaimName = printInfo.bind(person2, "kiel")
+let printMauName = printInfo.bind(person3, "deurne")
+
+console.log("\n bind \n ");
+
+printMoName()
+printNaimName()
+printMauName()
